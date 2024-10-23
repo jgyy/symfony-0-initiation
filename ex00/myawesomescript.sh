992e256ca9d6f795dc17071345f5f055062adb84
jegoh <jeffrey.goh@sit.singaporetech.edu.sg>
2024-10-23 09:57:00 +0800

    edited to minimum script for ex00

--- ex00/myawesomescript.sh
+++ ex00/myawesomescript.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <bit.ly_url>"
-    exit 1
-fi
-
-curl -sIL "$1" | \
-    grep "Location:" | \
-    tail -n 1 | \
-    cut -d' ' -f2
+curl -sI "$1" | grep "Location:" | cut -d " " -f 2
